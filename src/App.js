b737864ddbf0aae8f4eaaf2aa6373b47591c46ee
Swarup Hande <71242592+RoyalKing6868@users.noreply.github.com>
2023-10-07 18:42:09 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextBox from './components/TextBox';
+import Alert from './components/Alert'
+import React, {useState} from 'react';
 
 function App() {
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message , type) =>{
+    setAlert({
+      msg : message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null,null);
+    }, 2000);
+  }
+  const [mode, setMode] = useState("light");
+  const toggle = () =>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#000721';
+      showAlert("Dark Mode Enabled","success");
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = '#ffffff';
+      showAlert("Light Mode Enabled","success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar mode={mode} toggleMode={toggle} heading="TextUtils App"/>
+    <Alert alert={alert} />
+    <TextBox mode={mode} showAlert={showAlert}/>
+    </>
   );
 }
 
---
+++ src/components/TextBox.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+export default function TextBox(props) {
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleUpper = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to UpperCase", "success");
+  };
+  const handleLower = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to LowerCase", "success");
+  };
+  const handleCap = () => {
+    let newText = text.replace(/(^\w{1})|(\s+\w{1})/g, (letter) =>
+      letter.toUpperCase()
+    );
+    setText(newText);
+    props.showAlert("Converted to Capitalize", "success");
+  };
+  const handleRemoveSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Spaces Removed", "success");
+  };
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copied", "success");
+  };
+  const handleClear = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared", "success");
+  };
+  const [text, setText] = useState();
+  return (
+    <>
+      <div className={`container text-${props.mode==='light'?'black':'white'}`}>
+        <h1>Enter Text : </h1>
+        <div className="mb-3 my-2">
+          <textarea
+            className="form-control "
+            style={{backgroundColor : props.mode==='dark' ? '#000721':'white', color: props.mode==='light' ? 'black':'white'}}
+            id="myBox"
+            onChange={handleChange}
+            value={text}
+            rows="5"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpper}>
+          Upper Case
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleLower}>
+          Lower Case
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCap}>
+          Capitalize
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleRemoveSpace}>
+          Remove Extra Spaces
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCopy}>
+          Copy
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClear}>
+          Clear
+        </button>
+        <div className="summary">
+          <h2>Summary</h2>
+          <p>
+            {text?.split(" ").length - 1} Words, {text?.length} Characters
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
